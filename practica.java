7756e894e981840c63f455fc510f5d500b2604e0
ArturoJava <arturociscosystems@gmail.com>
2024-08-13 11:11:15 -0500

    estoy seguro de pasar este commit desde la Of

---
+++ practica.java
@@ -0,0 +1,15 @@
+
+public class practica {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 999; i = i + 1)
+		{
+		          
+		        System.out.println(" Hola usuario " +i+ " bienvenido ");
+		        
+		}
+
+	}
+
+}
